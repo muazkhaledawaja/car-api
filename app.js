83f8064ee044077e1d955122c291097f7bbd0d03
muazkhaledawaja <mkaawaja5000@gmail.com>
2022-09-24 10:35:40 +0300

    addinh mongo

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 const express = require('express')
 const carRoutes = require('./routes/cars')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 const app = express();
 
+mongoose.connect('mongodb://localhost:27017/carRent', {
 
+}, (err) => {
+    if (err) {
+        console.log(err)
+    } else(
+        console.log('successfully connected')
+    )
+})
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
 
 app.use(bodyParser.json())
 
--- controllers/cars.js
+++ controllers/cars.js
@@ -1,46 +1,62 @@
 const { v4: uuidv4 } = require('uuid');
+const Car = require('../models/car');
 
-let cars = [{
-    "name": "muaz",
-    "model": "RD",
-    "year": 2020,
-    "id": "3acfc4b5-66b1-4f4b-ab4d-54e00ed2ec92"
-}]
+//  
 
-module.exports.getCar = (req, res) => {
+module.exports.getCar = async(req, res) => {
+    const cars = await Car.find()
     res.send(cars)
-    console.log(cars);
 }
 
-module.exports.createCar = (req, res) => {
-    const car = req.body;
-    const carWithId = {...car, id: uuidv4() }
-    cars.push(carWithId)
-    res.send(`A Car with the name ${carWithId.name} added to the db`)
+module.exports.createCar = async(req, res) => {
+    // const car = req.body;
+    // const carWithId = {...car, id: uuidv4() }
+    // cars.push(carWithId)
+    // res.send(`A Car with the name ${carWithId.name} added to the db`)
+    const myCar = new Car(req.body);
+    await myCar.save()
+    res.send(myCar)
+    console.log(myCar);
+
 };
 
-module.exports.findCar = (req, res) => {
-    const { id } = req.params;
-    const test = cars.find(car => car.id == id)
-        //  const test = cars.find(car => car.id === id);
-    console.log(test)
-    res.send(test)
+module.exports.findCar = async(req, res) => {
+    const myCar = await Car.findById(req.params.id);
+    if (!myCar) {
+        res.send("can't find this car");
+    }
+    res.send(myCar)
+    console.log(myCar);
+
+
+    // const test = cars.find(car => car.id === id)
+    //     //  const test = cars.find(car => car.id === id);
+    // console.log(test)
+    // res.send(test)
 };
 
 module.exports.updateCar = async(req, res) => {
     const { id } = req.params;
-    const { name, model, year } = req.body
-    const car = cars.find((car) => { car.id == id })
+    const myCar = await Car.findByIdAndUpdate(id, {...req.body });
+    await myCar.save();
+    res.send(myCar)
 
-    if (name) car.name = name
-    if (model) car.model = model
-    if (year) car.year = year
-    res.send(`car with the id ${id} has been updated`)
+    // const { name, model, year } = req.body
+    // const car = cars.find((car) => { car.id == id })
+
+    // if (name) car.name = name
+    // if (model) car.model = model
+    // if (year) car.year = year
+    // res.send(`car with the id ${id} has been updated`)
 }
 
-module.exports.deleteCar = (req, res) => {
+module.exports.deleteCar = async(req, res) => {
     const { id } = req.params;
-    cars = cars.filter((car) => car.id != id)
-    res.send(`car with the id ${id} deleted from the DB`)
+    const myCar = await Car.findByIdAndDelete(id);
+    res.send(myCar)
+    console.log(myCar);
+
+    // cars = cars.filter((car) => car.id != id)
+    // res.send(`car with the id ${id} deleted from the DB`)
 
 }
